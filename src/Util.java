c7ca3ac0a7197a8c969c984c822827659d1353d2
Leon <cohenleonlbc@gmail.com>
2022-05-23 17:36:16 -0300

    Added Methods

--- src/Util.java
+++ src/Util.java
@@ -1,11 +1,33 @@
+import javax.swing.JOptionPane;
+
+import Exceptions.ObjectAlreadyExists;
+
 public class Util {
     public static void sellProduct() {
     }
 
-    public static void listProducts() {
+    public static void listProducts(OrderedList<Product> stock) {
+        JOptionPane.showMessageDialog(null, stock.listAll());
     }
 
-    public static void registerProduct() { 
+    public static void registerProduct(OrderedList<Product> stock) throws ObjectAlreadyExists{ 
+        String name = JOptionPane.showInputDialog("Product Name: ");
+        String expiration = JOptionPane.showInputDialog("Expiration Date (DD/MM/YY): ");
+        int stock_quantity = Integer.parseInt(JOptionPane.showInputDialog("Quantity in Stock: "));
+        
+        Product product = new Product(name, stock_quantity, expiration);
+        
+        try {
+            stock.Insert(product);
+        } catch (Exception e) {
+            throw new ObjectAlreadyExists();
+        }
+        
+        JOptionPane.showMessageDialog(null, "Product Added Successfully!");
+    }
+
+    public static String chooseOption() {
+        return JOptionPane.showInputDialog(Util.options());
     }
 
     public static String options() {
@@ -17,4 +39,6 @@ public class Util {
         options += "\n 4. Exit";
         return options;
     }
+
+
 }
