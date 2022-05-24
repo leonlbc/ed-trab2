3e89f37a5386e2d795f48e934fe75c64ce65f20d
Leon <cohenleonlbc@gmail.com>
2022-05-23 22:35:22 -0300

    Changed Methods & Attributes Modifier To Protected

---
+++ src/OrderedList/Node.java
@@ -0,0 +1,12 @@
+package OrderedList;
+public class Node<T>{
+
+    protected Node<T> right;
+    protected Node<T> left;
+    T data;
+
+    protected Node(T data) {
+        this.data = data;
+    }
+
+}
